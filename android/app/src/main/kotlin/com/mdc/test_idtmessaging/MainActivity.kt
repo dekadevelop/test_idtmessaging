b2b8ab032e0122764489d9da99379dc89994e2c1
dekadevelop <dekadevelop@gmail.com>
2020-09-27 00:16:46 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/mdc/test_idtmessaging/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mdc.test_idtmessaging
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'IDT Messaging',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Home Page"),
+        ),
+      ),
+    );
+  }
+}
