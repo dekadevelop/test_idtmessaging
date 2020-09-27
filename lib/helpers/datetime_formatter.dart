ae0a9ab4ec596fdf48f21b709b0bc2c15ef47898
dekadevelop <dekadevelop@gmail.com>
2020-09-27 12:31:30 +0200

    Design ConversationCard

--- lib/helpers/datetime_formatter.dart
+++ lib/helpers/datetime_formatter.dart
@@ -1,6 +1,6 @@
 String getDateFromTimeStamp(int value) {
   var d = DateTime.fromMillisecondsSinceEpoch(value);
-  return "${_format(d.day)}/${_format(d.month)}/${d.year} "
+  return "${d.day}/${_format(d.month)}/${d.year} "
       "${_format(d.hour)}:${_format(d.minute)}:${_format(d.second)}";
 }
 
--- lib/views/home_page/conversation_card.dart
+++ lib/views/home_page/conversation_card.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:test_idtmessaging/helpers/datetime_formatter.dart';
+import 'package:test_idtmessaging/helpers/random_helper.dart';
 import 'package:test_idtmessaging/models/conversation.dart';
 import 'package:test_idtmessaging/viewmodels/conversation_messages_viewmodel.dart';
 import 'package:test_idtmessaging/views/conversation_page/conversation_page.dart';
@@ -13,31 +15,90 @@ class ConversationCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => ListenableProvider(
-              create: (_) => ConversationMessagesViewModel(),
-              child: ConversationPage(conversation),
-            ),
-          ),
-        );
+        _goToConversation(context);
       },
       child: Card(
         child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+          child: Stack(
             children: [
-              Text(conversation.id),
-              Text(conversation.topic),
-              Text(conversation.lastMessage),
-              Text(conversation.members.toString()),
-              Text(conversation.modifiedAt.toString()),
+              Row(
+                children: [
+                  _avatar(),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: _mainContent(),
+                    ),
+                  ),
+                ],
+              ),
+              Positioned(
+                top: 0,
+                right: 0,
+                child: _modifiedAt(),
+              )
             ],
           ),
         ),
       ),
+    );
+  }
+
+  Future _goToConversation(BuildContext context) {
+    return Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => ListenableProvider(
+          create: (_) => ConversationMessagesViewModel(),
+          child: ConversationPage(conversation),
+        ),
+      ),
+    );
+  }
+
+  Container _modifiedAt() {
+    return Container(
+      width: 90,
+      child: Text(
+        getDateFromTimeStamp(conversation.modifiedAt),
+        textAlign: TextAlign.end,
+        maxLines: 2,
+        style: TextStyle(
+          fontStyle: FontStyle.italic,
+          fontSize: 12,
+        ),
+      ),
+    );
+  }
+
+  Container _avatar() {
+    return Container(
+      width: 60,
+      child: CircleAvatar(
+        radius: 30,
+        backgroundImage:
+            AssetImage('assets/avatars/avatar${getRandomInt(4)}.png'),
+      ),
+    );
+  }
+
+  Column _mainContent() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          conversation.topic,
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(height: 12),
+        Text(conversation.lastMessage),
+        SizedBox(height: 5),
+        Text(conversation.members.toString()),
+      ],
     );
   }
 }
