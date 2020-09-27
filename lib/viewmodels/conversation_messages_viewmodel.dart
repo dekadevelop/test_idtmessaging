ec6d1a4ab474b6d868489e46306edf71a3df5492
dekadevelop <dekadevelop@gmail.com>
2020-09-27 10:25:16 +0200

    Get Answer after 2 seconds the message was sent
    initial setup

--- lib/viewmodels/conversation_messages_viewmodel.dart
+++ lib/viewmodels/conversation_messages_viewmodel.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/foundation.dart';
 import 'package:test_idtmessaging/locator.dart';
 import 'package:test_idtmessaging/models/conversation_message.dart';
@@ -5,6 +7,7 @@ import 'package:test_idtmessaging/repositories/conversation_repo.dart';
 
 class ConversationMessagesViewModel with ChangeNotifier {
   ConversationRepo get repo => getIt.get<ConversationRepo>();
+  Timer timerAnswer;
 
   List<ConversationMessage> messages;
   ConversationMessagesViewModel() : messages = [];
@@ -17,5 +20,22 @@ class ConversationMessagesViewModel with ChangeNotifier {
   sendMessage(String message) {
     messages.add(ConversationMessage(id: '123', message: message));
     notifyListeners();
+    startTimerAnswer();
+  }
+
+  getRandomAnswer() {
+    messages.add(ConversationMessage(id: '123', message: 'randomMessage'));
+    notifyListeners();
+  }
+
+  void startTimerAnswer() {
+    timerAnswer = Timer(
+      Duration(seconds: 2),
+      (() => getRandomAnswer()),
+    );
+  }
+
+  void stopTimerAnswer() {
+    if (timerAnswer != null) timerAnswer.cancel();
   }
 }
