1c659d398bf8a1b42b21ee93c14d1273b27ef256
dekadevelop <dekadevelop@gmail.com>
2020-09-27 10:39:39 +0200

    MessagesList: scroll to bottom initially and when a message is received

--- lib/views/conversation_page/messages_list.dart
+++ lib/views/conversation_page/messages_list.dart
@@ -1,20 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:provider/provider.dart';
 import 'package:test_idtmessaging/viewmodels/conversation_messages_viewmodel.dart';
 
 import 'conversation_message_card.dart';
 
-class MessagesList extends StatelessWidget {
+class MessagesList extends StatefulWidget {
+  @override
+  _MessagesListState createState() => _MessagesListState();
+}
+
+class _MessagesListState extends State<MessagesList> {
+  ScrollController _scrollController = new ScrollController();
+
+  @override
+  void initState() {
+    _scrollController = new ScrollController();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     var viewModel = Provider.of<ConversationMessagesViewModel>(context);
+
+    SchedulerBinding.instance.addPostFrameCallback((_) {
+      _scrollController.jumpTo(_scrollController.position.maxScrollExtent);
+    });
+
     return ListView.builder(
       padding: const EdgeInsets.all(8),
+      controller: _scrollController,
       itemCount: viewModel.messages.length,
       itemBuilder: (BuildContext context, int index) {
         return ConversationMessageCard(viewModel.messages[index]);
       },
     );
-    ;
   }
 }
